9cf2d5ad5cc94778e5d1f95562c50c1c5a996d4f
Vivek <vivek.patani@somaiya.edu>
2016-01-31 13:24:43 -0500

    Basic Form Layout

--- index.php
+++ index.php
@@ -1,14 +1,64 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Turn Key Continuous Integration</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
-  </body>
+<head>
+	<title>Turn Key Continuous Integration</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php require 'header.php' ?>
+</head>
+<body>
+
+	<!-- Navigator for the website, Home is active. Once finalised override -->
+	<nav class="navbar navbar-inverse">
+
+		<!-- To cover the complete width -->
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="index.php">UITS Project Gen 0.9</a>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="index.php">Home</a></li>
+					</ul>
+				</div>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+
+	<div class="container">
+		<form name="project_form" id="project_form">
+			<div class="row col-lg-12">
+				<div class="form-group col-lg-6">
+					<h2 class="text-left">Project Metadata</h2>
+					<p class="text-left">Artifact Coordinates</p>
+					<br>
+					<label for="project_group">Project Group</label>
+					<input type="text" class="form-control" id="project_group" placeholder="Project Group">
+				</div>
+				<div class="form-group col-lg-6">
+					<h2 class="text-left">Project Metadata</h2>
+					<p class="text-left">Artifact Coordinates</p>
+					<br>
+					<label for="project_dependancies">Add Dependancies</label>
+					<input type="text" class="form-control" id="project_dependancies" placeholder="Enter dependancies sepeareted by commas">
+				</div>
+			</div>
+		</div>
+		<div class="form-group row text-center col-lg-12">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-secondary">Sign in</button>
+			</div>
+		</div>
+	</form>	
+</div>
+
+
+<?php require 'imports.php' ?>
+
+</body>
+<footer>
+	<?php require 'footer.php' ?>
+</footer>
 </html>
